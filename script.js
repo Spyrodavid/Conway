53b074d1d2041206be0a4245f2e40ae33cb5678c
Spyrodavid <78068424+Spyrodavid@users.noreply.github.com>
2021-04-11 18:54:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function buffer(){
 
 function clear(){
 	clearInterval(interval)
-	for (x of [0,0]
+	for (p of [0,0]){
 	board.childNodes.forEach(cell=>{
 		for (x of cell.classList){
 			 if (x != 'buffer' && x != 'cell'){
@@ -220,4 +220,5 @@ function clear(){
 			}
 		}
 	})
+	}
 }
